3b5d2d37f7dd74941260c5f17d56479db362c057
RajashreeParida <33974983+RajashreeParida@users.noreply.github.com>
2017-11-25 12:46:50 +0530

    Update FlightBookingTest.java

--- src/main/java/FlightBookingTest.java
+++ src/main/java/FlightBookingTest.java
@@ -29,21 +29,25 @@ public class FlightBookingTest {
         //wait for the auto complete options to appear for the origin
 
         waitFor(2000);
-        List<WebElement> originOptions = driver.findElement(By.id("ui-id-1")).findElements(By.tagName("li"));
+        //List<WebElement> originOptions = driver.findElement(By.id("ui-id-1")).findElements(By.tagName("li"));
+        List<WebElement> originOptions = driver.findElement(By.id("ui-id-1")).findElements(By.xPath("li/a"));
         originOptions.get(0).click();
 
-        driver.findElement(By.id("toTag")).clear();
-        driver.findElement(By.id("toTag")).sendKeys("Delhi");
+       // driver.findElement(By.id("toTag")).clear();
+        //driver.findElement(By.id("toTag")).sendKeys("Delhi");
+        driver.findElement(By.id("ToTag")).clear();
+        driver.findElement(By.id("ToTag")).sendKeys("Delhi");
 
         //wait for the auto complete options to appear for the destination
 
         waitFor(2000);
         //select the first item from the destination auto complete list
-        List<WebElement> destinationOptions = driver.findElement(By.id("ui-id-2")).findElements(By.tagName("li"));
+       // List<WebElement> destinationOptions = driver.findElement(By.id("ui-id-2")).findElements(By.tagName("li"));
+         List<WebElement> destinationOptions = driver.findElement(By.id("ui-id-2")).findElements(By.xPath("li/a"));
         destinationOptions.get(0).click();
 
-        driver.findElement(By.xpath("//*[@id='ui-datepicker-div']/div[1]/table/tbody/tr[3]/td[7]/a")).click();
-
+       // driver.findElement(By.xpath("//*[@id='ui-datepicker-div']/div[1]/table/tbody/tr[3]/td[7]/a")).click();
+        driver.findElement(By.xpath("//*[@id='ui-datepicker-div']/div[1]/table/tbody/tr/td/a[text()='26']")).click();
         //all fields filled in. Now click on search
         driver.findElement(By.id("SearchBtn")).click();
 
